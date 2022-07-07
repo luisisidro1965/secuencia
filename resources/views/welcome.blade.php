595ac11edfef48fa5c681a47e00513db5a461d4c
luisisidro <luisalfredoisidro@gmail.com>
2022-07-06 23:47:00 -0500

    Travbajar con Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
-
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
@@ -18,6 +17,8 @@
     <div class="container">
         <br>
        <h1>Estoy en Welcome</h1>
+       <input type="text" name="" id="">
+       <input type="button" value="Enviar">
     </div>
 </body>
 
